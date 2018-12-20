28445c352268325c7349ef3369daf07f11d96143
Palash Nigam <npalash25@gmail.com>
2018-12-20 20:25:50 +0530

    bump with correct checksum

--- abc.rb
+++ abc.rb
@@ -1,12 +1,12 @@
 class Abc < Formula
   desc "Power of appbase.io via CLI, with nifty imports from your favorite data sources"
   homepage "https://github.com/appbaseio/abc"
-  url "https://github.com/appbaseio/abc/releases/download/0.6.6/abc-darwin-0.6.6.zip"
-  sha256 "8daba3d06a7a8a85777e765c97850389f361af62a4071cdb72729ff0c9e20bc4"
+  url "https://github.com/appbaseio/abc/releases/download/0.6.7/abc-linux-0.6.7.zip"
+  sha256 "b13ed26b0f0fd457adcd7d58c53a2ee5017ac253e429c078e9c5a6a97ab4406f"
 
   def install
-    bin.install "abc-0.6.6"
-    mv "#{bin}/abc-0.6.6", "#{bin}/abc"
+    bin.install "abc-0.6.7"
+    mv "#{bin}/abc-0.6.7", "#{bin}/abc"
   end
 
   test do
