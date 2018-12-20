f6bfcf1d7ac54d1f478d051df7ead81753b3a64d
Palash Nigam <npalash25@gmail.com>
2018-12-20 20:30:36 +0530

    darwin binary bump

--- abc.rb
+++ abc.rb
@@ -1,7 +1,7 @@
 class Abc < Formula
   desc "Power of appbase.io via CLI, with nifty imports from your favorite data sources"
   homepage "https://github.com/appbaseio/abc"
-  url "https://github.com/appbaseio/abc/releases/download/0.6.7/abc-linux-0.6.7.zip"
+  url "https://github.com/appbaseio/abc/releases/download/0.6.7/abc-darwin-0.6.7.zip"
   sha256 "b13ed26b0f0fd457adcd7d58c53a2ee5017ac253e429c078e9c5a6a97ab4406f"
 
   def install
