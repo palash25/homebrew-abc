a209cc330f08dbf5d33327664166e717ee0f5a4e
Palash Nigam <npalash25@gmail.com>
2019-04-18 21:06:48 +0530

    0.10.2

--- abc.rb
+++ abc.rb
@@ -1,12 +1,12 @@
 class Abc < Formula
   desc "Power of appbase.io via CLI, with nifty imports from your favorite data sources"
   homepage "https://github.com/appbaseio/abc"
-  url "https://github.com/appbaseio/abc/releases/download/0.6.7/abc-darwin-0.6.7.zip"
-  sha256 "b13ed26b0f0fd457adcd7d58c53a2ee5017ac253e429c078e9c5a6a97ab4406f"
+  url "https://github.com/appbaseio/abc/releases/download/0.10.2/abc-darwin-0.10.2.zip"
+  sha256 "d4a554a2c689d427ede981707fc61f128227d497c2404c2831ea766f6a9ea403"
 
   def install
-    bin.install "abc-0.6.7"
-    mv "#{bin}/abc-0.6.7", "#{bin}/abc"
+    bin.install "abc-0.10.2"
+    mv "#{bin}/abc-0.10.2", "#{bin}/abc"
   end
 
   test do
